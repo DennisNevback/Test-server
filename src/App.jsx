16e0043538f3cc6a3047be6df3733a9a62a0bc93
DennisNevback <dennis.nevback@gmail.com>
2023-03-03 11:59:43 +0100

    dawda

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Home } from './assets/Pages/home'
+import { Home } from '/assets/Pages/home'
 
 function App() {
   const [count, setCount] = useState(0)
